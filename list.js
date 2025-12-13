3fe39e86bfe40519626f9cc3f8a01a2d904e4fb8
Manaralkuwaiti <manaralkuwaiti146@gmail.com>
2025-12-13 22:54:54 +0300

    Update list.js

--- list.js
+++ list.js
@@ -1,47 +1,28 @@
-/*
-  Requirement: Populate the "Weekly Course Breakdown" list page.
+const listSection = document.querySelector('#resource-list-section');
 
-  Instructions:
-  1. Link this file to `list.html` using:
-     <script src="list.js" defer></script>
+function createResourceArticle(resource) {
+  const article = document.createElement('article');
 
-  2. In `list.html`, add an `id="week-list-section"` to the
-     <section> element that will contain the weekly articles.
+  article.innerHTML = `
+    <h3>${resource.title}</h3>
+    <p>${resource.description || ''}</p>
+    <a href="details.html?id=${resource.id}">
+      View Resource & Discussion
+    </a>
+  `;
 
-  3. Implement the TODOs below.
-*/
-
-// --- Element Selections ---
-// TODO: Select the section for the week list ('#week-list-section').
-
-// --- Functions ---
-
-/**
- * TODO: Implement the createWeekArticle function.
- * It takes one week object {id, title, startDate, description}.
- * It should return an <article> element matching the structure in `list.html`.
- * - The "View Details & Discussion" link's `href` MUST be set to `details.html?id=${id}`.
- * (This is how the detail page will know which week to load).
- */
-function createWeekArticle(week) {
-  // ... your implementation here ...
+  return article;
 }
 
-/**
- * TODO: Implement the loadWeeks function.
- * This function needs to be 'async'.
- * It should:
- * 1. Use `fetch()` to get data from 'weeks.json'.
- * 2. Parse the JSON response into an array.
- * 3. Clear any existing content from `listSection`.
- * 4. Loop through the weeks array. For each week:
- * - Call `createWeekArticle()`.
- * - Append the returned <article> element to `listSection`.
- */
-async function loadWeeks() {
-  // ... your implementation here ...
+async function loadResources() {
+  const response = await fetch('resources.json');
+  const resources = await response.json();
+
+  listSection.innerHTML = '';
+
+  resources.forEach(res => {
+    listSection.appendChild(createResourceArticle(res));
+  });
 }
 
-// --- Initial Page Load ---
-// Call the function to populate the page.
-loadWeeks();
+loadResources();
